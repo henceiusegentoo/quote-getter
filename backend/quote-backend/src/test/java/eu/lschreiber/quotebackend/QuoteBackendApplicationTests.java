ffaa3e408c6b2e549ebb3192d7dd244b40fed241
henceiusegentoo <61363222+henceiusegentoo@users.noreply.github.com>
2025-03-25 15:45:24 +0100

    add initial backend setup with Spring Boot and Maven

---
+++ backend/quote-backend/src/test/java/eu/lschreiber/quotebackend/QuoteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.lschreiber.quotebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
