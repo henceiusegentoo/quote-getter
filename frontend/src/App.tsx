aaaa1348598d8b8f4b9b0b55b81602da27170d4d
henceiusegentoo <61363222+henceiusegentoo@users.noreply.github.com>
2025-03-26 13:03:34 +0100

    add quote fetching functionality and update app title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
+import React from 'react'
+
 function App() {
-  return (
-    <>
-        <h1>
-            Hello World
-        </h1>
-    </>
-  )
+    const [quote, setQuote] = React.useState('')
+
+    const fetchQuote = async () => {
+        const response = await fetch('http://127.0.0.1:8080/api/quote')
+        console.log(response)
+
+    }
+
+    return (
+        <>
+            <h1>
+                Hello World
+            </h1>
+            <button onClick={fetchQuote}>
+                Get Quote
+            </button>
+        </>
+    )
 }
 
 export default App
